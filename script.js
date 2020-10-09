58f3c6e2d1a381df6d75101130a5620d37dce97d
annima24 <nm.andrew24@gmail.com>
2020-10-09 09:51:56 -0400

    it's working

--- script.js
+++ script.js
@@ -1,61 +1,67 @@
 // This function will return a random value of either rock, paper, or scissor
 
+
+
+const b1 = document.querySelector('#b1');
+const b2 = document.querySelector('#b2');
+const b3 = document.querySelector('#b3');
 const choices = ['rock', 'paper', 'scissor']
 let playerScore = 0
 let computerScore = 0
+const readOut = document.querySelector('#result')
+const tally = document.querySelector('#tally')
+const winner = document.querySelector('#winner') ;
+
+
 
 computerPlay = () =>    {
     // Cycles through the choices array and returns a random choice
     const random = Math.floor(Math.random() * choices.length);
     return (choices[random]);
-    
 }  
+
+
 // Takes two values and compares them and returns a winner based on the ancient rules
-const playRound = (playerSelection, computerSelection) =>   {
-    // calls for user to input their selection
-    let userChoice= prompt('Choose either Rock, Paper, or Scissor');
-    // storing prompt results in a variable
-    let userChoiceResults = userChoice
-    // formatting to lowercase
-    playerSelection =userChoiceResults.toLowerCase();
-    // running computerPlay function and storing the result
+function playRound (playerSelection, computerSelection)  {
     computerSelection = computerPlay();
+        if (playerScore === 3) {
+            return winner.textContent = `After 5 rounds you had ${playerScore} wins, and your opponent had ${computerScore} wins, you have crushed the enemy and stand victorious!`;
+         }   else if (computerScore === 3)   {
+             return winner.textContent = `After 5 rounds you had ${playerScore} wins, and your opponent had ${computerScore} wins, the machines have won, you have failed the human race.`;
+         }
     // checking conditions to see if it's a tie
-    if  (playerSelection === 'rock' && computerSelection ==='rock'
+        else if  
+        (playerSelection === 'rock' && computerSelection ==='rock'
     ||   playerSelection === 'paper' && computerSelection ==='paper'
     ||   playerSelection === 'scissor' && computerSelection ==='scissor')            
     {
-        return (console.log('The computer chose '+computerSelection+', and you chose '+playerSelection+ ', so its a tie!'));
+        return readOut.textContent = 'The computer chose '+computerSelection+', and you chose '+playerSelection+ ', so its a tie!', tally.textContent = `your score is ${playerScore} and your opponents score is ${computerScore}`;
         // checking conditions to see if player wins
     }   else if  
         (playerSelection === 'rock' && computerSelection ==='scissor'
     ||   playerSelection === 'paper' && computerSelection ==='rock'
     ||   playerSelection === 'scissor' && computerSelection ==='paper')            
     {
-        return (console.log('The computer chose '+computerSelection+', and you chose '+playerSelection+ ', so you win!'), playerScore++);
+        return readOut.textContent = 'The computer chose '+computerSelection+', and you chose '+playerSelection+ ', so you win!' , playerScore++, tally.textContent = `your score is ${playerScore} and your opponents score is ${computerScore}`;
         // checking conditions to see if player loses
     }   else if  
         (playerSelection === 'scissor' && computerSelection ==='rock'
     ||   playerSelection === 'rock' && computerSelection ==='paper'
     ||   playerSelection === 'paper' && computerSelection ==='scissor')            
     {
-    return (console.log('The computer chose '+computerSelection+', and you chose '+playerSelection+ ', so you lose!'), computerScore++);
-}   else return (console.log('an error occured'))
-}
-// game function which takes two paramaters, and after 5 rounds returns the winner
-const game = () =>  {
-    playRound('', computerPlay)
-    playRound('', computerPlay)
-    playRound('', computerPlay)
-    playRound('', computerPlay)
-    playRound('', computerPlay)
-  if (playerScore>computerScore)  {
-    return (`After 5 rounds you had ${playerScore} wins, and your opponent had ${computerScore} you have crushed the enemy and stand victorious!`)
-}   else if (playerScore<computerScore) {
-    return (`After 5 rounds you had ${playerScore} wins, and your opponent had ${computerScore} wins, the machines have won, you have failed the human race.`)
-}   else return (`After 5 rounds you had ${playerScore} wins, and your opponent had ${computerScore} wins, it's a tie!`)
+        return readOut.textContent = 'The computer chose '+computerSelection+', and you chose '+playerSelection+ ', so you lose!' , computerScore++, tally.textContent = `your score is ${playerScore} and your opponents score is ${computerScore}`;
+        // if none of the above conditions are met an error is returned.
+    }   else return readOut.textContent = 'an error occured'
+
 }
 
-
-console.log(game());
+b1.addEventListener('click', () => {
+    playRound('rock')
+})
+b2.addEventListener('click', () => {
+    playRound('paper')
+})
+b3.addEventListener('click', () => {
+    playRound('scissor')
+})
 
